c7a2bca50924bff88dab00221fa61b3a5005d228
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-11-15 17:20:41 -0800

    working

---
+++ image_class_map.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+# NAME: image_class_map.py
+"""
+AUTHOR: Ian Chavez
+
+Unpublished-rights reserved under the copyright laws of the United States.
+
+This data and information is proprietary to, and a valuable trade secret of Ian Chavez. It is given in confidence by
+Ian Chavez. Its use, duplication, or disclosure is subject to the restrictions set forth in the License Agreement under which it has been
+distributed.
+
+Unpublished Copyright © 2023 Ian Chavez
+
+All Rights Reserved
+"""
+"""
+========================== MODIFICATION HISTORY ==============================
+11/15/23:
+MOD: Creation of file and initial function
+AUTHOR: Ian Chavez
+COMMENT: n/a
+====================== END OF MODIFICATION HISTORY ============================
+"""
+import os
+import csv
+
+# Set paths
+data_dir = "./dataset/"
+csv_file_path = "image_class_map.csv"
+
+# If csv file exists, clear it
+if os.path.exists(csv_file_path):
+    open(csv_file_path, "w").close()
+
+# Write to csv file, img name (i.e. 0001.jpg), path, and class (root folder)
+with open("image_class_map.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    field = ["Image", "Path", "Class"]
+    writer.writerow(field)
+
+    for root, dirs, files in os.walk(data_dir):
+        for name in files:
+            if name.endswith(".jpg"):
+                path = os.path.join(root, name)
+                class_name = os.path.basename(root)
+                writer.writerow([name, path, class_name])
--- preprocess.py
+++ preprocess.py
@@ -21,51 +21,66 @@ AUTHOR: Ian Chavez
 COMMENT: n/a
 ====================== END OF MODIFICATION HISTORY ============================
 """
+print("Importing libraries...")
 import os
 import shutil
 import random
+import torch
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 
+print("Setting paths...")
 # Set the paths
-data_dir = "CNN_weatherRecognition/dataset/"
-train_dir = "CNN_weatherRecognition/data_preprocessing/train/"
-val_dir = "CNN_weatherRecognition/data_preprocessing/validation/"
-test_dir = "CNN_weatherRecognition/data_preprocessing/test/"
+data_dir = "./dataset/"
+processed_datasets_dir = "./processed_datasets/"
+train_dir = "./processed_datasets/train/"
+val_dir = "./processed_datasets/validation/"
+test_dir = "./processed_datasets/test/"
 
+print("Creating directories...")
 # Create directories for train, validation, and test sets
+os.makedirs(processed_datasets_dir, exist_ok=True)
 os.makedirs(train_dir, exist_ok=True)
 os.makedirs(val_dir, exist_ok=True)
 os.makedirs(test_dir, exist_ok=True)
 
-# Define the transformation
+print("Composing transformations...")
+# Transformations to be applied to the dataset
 transform = transforms.Compose(
     [
         transforms.Resize((224, 224)),
+        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
     ]
 )
 
-# Load the dataset
+print("Loading dataset w transformations...")
+# Load dataset and apply transformations
 dataset = ImageFolder(root=data_dir, transform=transform)
 
+print("Calculating dataset sizes...")
 # Calculate dataset sizes
 total_size = len(dataset)
 train_size = int(0.7 * total_size)
 val_size = int(0.15 * total_size)
 test_size = total_size - train_size - val_size
 
+print("Splitting dataset...")
 # Split the dataset
 train_set, val_set, test_set = torch.utils.data.random_split(
     dataset, [train_size, val_size, test_size]
 )
 
+print("Creating data loaders...")
 # Create data loaders
 train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
 val_loader = DataLoader(val_set, batch_size=64, shuffle=False)
 test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
+
+
+print("Saving datasets...")
 
 
 # Save the split datasets to respective directories
